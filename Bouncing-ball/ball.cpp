d86b9313f420886118c3a4c9bab362b7a346cfbb
Federico Lucchi <federico.lucchi07@gmail.com>
2024-03-20 00:46:52 +0100

    added rotated, sqeezed ball

--- Bouncing-ball/ball.cpp
+++ Bouncing-ball/ball.cpp
@@ -1,6 +1,10 @@
 #include "ball.h"
+#include <chrono>
+#include <iostream>
 
 Ball::Ball()
+	:
+	body(sf::PrimitiveType::TriangleFan)
 {
 }
 
@@ -8,7 +12,9 @@ int Ball::run()
 {
 	const auto screenSize = sf::VideoMode::getDesktopMode();
 
-	w.create(sf::VideoMode(screenSize.width * 2/3, screenSize.width * 3 / 8), "Bouncing ball");
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	w.create(sf::VideoMode(screenSize.width * 2/3, screenSize.width * 3 / 8), "Bouncing ball", sf::Style::Default, settings);
 	w.setView(sf::View(sf::Vector2f(1920/2, 1080/2), sf::Vector2f(1920, 1080)));
 	w.setFramerateLimit(100);
 
@@ -21,8 +27,41 @@ int Ball::run()
 
 		w.clear(sf::Color(130, 130, 130));
 
+		update();
+		draw();
+
 		w.display();
 	}
 
 	return 0;
+}
+
+void Ball::update()
+{
+	float length = sqrt(area / PI * squeezed);
+	float height = sqrt(area / PI / squeezed);
+	float ang = atan(vel.y / vel.x);
+
+	body.clear();
+	body.append(sf::Vertex(pos, sf::Color::Black));
+	for (int i = 0; i < sidesBall + 2; i++) {
+		float circleAng = i / (sidesBall / 2.f) * PI;
+		float x = cos(circleAng) * length, y = sin(circleAng) * height;
+		float dist = sqrt(std::pow(x, 2) + std::pow(y, 2));
+		float pointAng = atan(y / x);
+		if (x < 0)
+			pointAng += PI;
+
+		sf::Vertex v;
+		v.position.x = pos.x + cos(ang + pointAng) * dist;
+		v.position.y = pos.y + sin(ang + pointAng) * dist;
+
+		v.color = sf::Color::Black;
+		body.append(v);
+	}
+}
+
+void Ball::draw()
+{
+	w.draw(body);
 }
--- Bouncing-ball/ball.h
+++ Bouncing-ball/ball.h
@@ -1,13 +1,24 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+#define PI 3.1415
+
 class Ball
 {
 public:
 	Ball();
 	int run();
+	void update();
+	void draw();
 
 private:
 	sf::RenderWindow w;
+
+	sf::VertexArray body;
+	sf::Vector2f pos = sf::Vector2f(540, 540);
+	sf::Vector2f vel = sf::Vector2f(0, 1);
+	float area = 100'000;
+	float squeezed = 2;
+	const int sidesBall = 40;
 };
 
